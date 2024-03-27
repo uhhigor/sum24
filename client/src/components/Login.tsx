92606bdf6de7323ba9865b1abe50277a1cbb8c2c
Tristram02 <wldoekpl@gmail.com>
2024-03-27 09:30:28 +0100

    Connect front with back

--- client/src/components/Login.tsx
+++ client/src/components/Login.tsx
@@ -1,29 +1,44 @@
 import React, { useState } from "react";
 import '../styles/login.css';
 import { Link } from "react-router-dom";
+import axios from "axios";
 
 export const Login = () => {
 
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
+    const [user, setUser] = useState({ username: '', password: ''})
+    
+    const handleChange = (e: any) => {
+        setUser({ ...user, [e.target.name]: e.target.name });
+    }
+
+    const login = () => {
+        axios.post('http://localhost:8080/login', user)
+            .then(response => {
+                console.log("Login complete");
+            })
+            .catch(error => {
+                console.error(error);
+            })
+    }
     
     return (
         <div className="login-container">
             <div className="col-4 login-form">
                 <h1 className="mt-5 ms-4 align-self-start">LOGIN</h1>
                 <input 
-                onChange={(e) => setEmail(e.target.value)}
+                onChange={handleChange}
                 className="form-control email mt-5" 
                 placeholder="Enter email" 
                 type="email" name="email" id="email" />
                 
                 <input
-                onChange={(e) => setPassword(e.target.value)} 
+                onChange={handleChange}
                 className="form-control password mt-3" 
                 placeholder="Enter password" 
                 type="password" name="password" id="pass" />
 
                 <button
+                onClick={login}
                 className="btn mt-5">Log in</button>
 
                 <span className="mt-5">Do not have account? <Link to={'/signup'}>Sign up</Link></span>
--- client/src/components/Signup.tsx
+++ client/src/components/Signup.tsx
@@ -1,37 +1,45 @@
 import React, { useEffect, useState } from "react";
 import "../styles/login.css";
 import { Link } from "react-router-dom";
+import axios from "axios";
 
 export const Signup = () => {
 
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
-    const [username, setUsername] = useState('')
+    const [user, setUser] = useState({ username: '', password: ''})
+    
+    const handleChange = (e: any) => {
+        setUser({ ...user, [e.target.name]: e.target.name });
+    }
+
+    const register = () => {
+        axios.post('http://localhost:8080/register', user)
+            .then(response => {
+                console.log("Register complete");
+            })
+            .catch(error => {
+                console.error(error);
+            })
+    }
     
     return (
         <div className="login-container">
             <div className="col-4 login-form">
                 <h1 className="mt-5 ms-4 align-self-start">SIGN UP</h1>
                 <input 
-                onChange={(e) => setUsername(e.target.value)}
+                onChange={handleChange}
                 className="form-control email mt-5" 
                 placeholder="Enter username" 
                 type="text" name="username" id="username" />
                 
-                <input 
-                onChange={(e) => setEmail(e.target.value)}
-                className="form-control email mt-3" 
-                placeholder="Enter email" 
-                type="email" name="email" id="email" />
-                
                 <input
-                onChange={(e) => setPassword(e.target.value)} 
+                onChange={handleChange} 
                 className="form-control password mt-3" 
                 placeholder="Enter password" 
                 type="password" name="password" id="pass" />
 
                 <button
-                className="btn btn-success mt-5">Sign up</button>
+                onClick={register}
+                className="btn mt-5">Sign up</button>
 
                 <span className="mt-5">Already have account? <Link to={'/login'}>Log in</Link></span>
             </div>
