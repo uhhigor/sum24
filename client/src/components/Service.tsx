98502f599d36f7347189076b72039758ec6e0431
Agnieszka Stasiak <242531@edu.p.lodz.pl>
2024-03-27 08:05:40 +0100

    better mocks, ping works for now

--- client/src/components/Service.tsx
+++ client/src/components/Service.tsx
@@ -17,31 +17,51 @@ interface ServiceProps {
 
 export const Service: React.FC<ServiceProps> = ({index}) => {
 
-    // const [service, setService] = useState<Service | null>(null);
-    const [services, setServices] = useState<Service[]>([]);
-    // useEffect(() => {
-    //     // Fetch service data when component mounts
-    //     fetchServiceData();
-    // }, []);
-    //
-    // const fetchServiceData = async () => {
-    //     try {
-    //         const response = await fetch(`http://localhost:8080/services/${index}`);
-    //         if (!response.ok) {
-    //             console.error('Failed to fetch service:', response);
-    //         }
-    //         const data = await response.json();
-    //         setService(data);
-    //     } catch (error) {
-    //         console.error('Error fetching service:', error);
-    //     }
-    // };
+    const [service, setService] = useState<Service | null>(null);
+    const [status, setStatus] = useState<boolean | null >(false);
+
+    useEffect(() => {
+        fetchServiceData();
+        setService({ id: 1, name: "Home", address: "https://p.lodz.pl/" })
+    }, []);
+
+    useEffect(() => {
+        const intervalId = setInterval(fetchServiceStatus, 30 * 1000);
+        return () => clearInterval(intervalId);
+    }, []);
+
+    const fetchServiceData = async () => {
+        try {
+            const response = await fetch(`http://localhost:8080/services/${index}`);
+            if (!response.ok) {
+                console.error('Failed to fetch service:', response);
+            }
+            const data = await response.json();
+            setService(data);
+        } catch (error) {
+            console.error('Error fetching service:', error);
+        }
+    };
+
+    const fetchServiceStatus = async () => {
+        try {
+            const response = await fetch(service?.address as string);
+            setStatus(response.status === 200);
+            console.log(response.status);
+        } catch (error) {
+            console.error('Error fetching service status:', error);
+        }
+    };
+
 
     function imageRender() {
-        if (true) {
+        console.log(status);
+        if (status === true) {
             return <FcApproval size={20}/>
-        } else {
+        } else if (status === false) {
             return <FcHighPriority size={20}/>
+        } else {
+            return <div>Unknown</div>
         }
     }
 
@@ -83,8 +103,8 @@ export const Service: React.FC<ServiceProps> = ({index}) => {
 
     return (
         <div className="m-2 service-container">
-            <h5>Service name</h5>
-            <p>Service address</p>
+            <h5>{service?.name}</h5>
+            <p>{service?.address}</p>
             <Row className="align-items-center justify-content-center">
                 <Col>
                     <div>Status:</div>
@@ -94,34 +114,12 @@ export const Service: React.FC<ServiceProps> = ({index}) => {
                 </Col>
             </Row>
             <div className="mt-4">
-                <Link to={"https://ftims.edu.p.lodz.pl/"}>
+                <Link to={service?.address as string} target="_blank" rel="noopener noreferrer">
                     <button className="btn">View</button>
                 </Link>
                 <button className="m-lg-3 btn" onClick={() => editService(index, "mockData")}>Edit</button>
                 <button className="btn" onClick={() => deleteService(index)}>Delete</button>
             </div>
-
-            {/*{service && (*/}
-            {/*    <>*/}
-            {/*        <h5>{service.name}</h5>*/}
-            {/*        <p>{service.address}</p>*/}
-            {/*        <Row className="align-items-center justify-content-center">*/}
-            {/*            <Col>*/}
-            {/*                <div>Status:</div>*/}
-            {/*            </Col>*/}
-            {/*            <Col>*/}
-            {/*                {imageRender()}*/}
-            {/*            </Col>*/}
-            {/*        </Row>*/}
-            {/*        <div className="mt-4">*/}
-            {/*            <Link to={service.address}>*/}
-            {/*                <button className="btn">View</button>*/}
-            {/*            </Link>*/}
-            {/*            <button className="m-lg-3 btn" onClick={() => editService(index, "mockData")}>Edit</button>*/}
-            {/*            <button className="btn" onClick={() => deleteService(index)}>Delete</button>*/}
-            {/*        </div>*/}
-            {/*    </>*/}
-            {/*)}*/}
 
         </div>
     );
