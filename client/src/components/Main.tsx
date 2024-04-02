711c9c2d92324d5ae8777edc3a8f1fc921855065
Agnieszka Stasiak <242531@edu.p.lodz.pl>
2024-04-02 13:11:48 +0200

    main added

--- client/src/components/Main.tsx
+++ client/src/components/Main.tsx
@@ -1,9 +1,58 @@
-import React from "react";
+import React, {useState} from "react";
+import "../styles/main.css";
+import {useNavigate} from "react-router-dom";
 
-export const Main = () => {
+export const Main : React.FC = () => {
+    const [isButtonHovered, setIsButtonHovered] = useState(false);
+    let navigate = useNavigate();
+
+    const handleButtonHover = () => {
+        setIsButtonHovered(true);
+    };
+
+    const handleButtonLeave = () => {
+        setIsButtonHovered(false);
+    };
+
+    const goToLoginPage = () => {
+        const path = `/login`;
+
+        navigate(path);
+    };
+    const goToRegisterPage = () => {
+        const path = `/signup`;
+        navigate(path)
+    };
+
     return (
-        <div>
-            Main
+        <div className="main-container">
+            <div className="d-flex justify-content-center align-items-center min-vh-100">
+                <div className="container-fluid">
+                    <div className="row">
+                        <div className="col text-center">
+                            <div className={`welcomeText ${isButtonHovered ? 'paused' : ''}`}>Welcome!
+                            </div>
+                            <button
+                                className="btn btn-lg m-5"
+                                onMouseEnter={handleButtonHover}
+                                onMouseLeave={handleButtonLeave}
+                                onClick={goToRegisterPage}
+                            >
+                                Register here!
+                            </button>
+                            <button
+                                className="btn btn-lg m-5"
+                                onMouseEnter={handleButtonHover}
+                                onMouseLeave={handleButtonLeave}
+                                onClick={goToLoginPage}
+                            >
+                                Log in here!
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
+
     )
 }
