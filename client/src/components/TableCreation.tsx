afa74ad08262921e6fd95fadf9395d8bb272dc3b
MaciejPerdaszek <maciej.44@wp.pl>
2024-04-03 21:01:33 +0200

    Service selection

--- client/src/components/TableCreation.tsx
+++ client/src/components/TableCreation.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, {useEffect, useState} from 'react';
 import axios from 'axios';
 
 export const TableCreation = () => {
@@ -7,7 +7,11 @@ export const TableCreation = () => {
 
     const [rowsToShow, setRowsToShow] = useState(10);
 
-    const [service, setService] = useState('table');
+    const [services, setServices] = useState([]);
+
+    useEffect(() => {
+        getServices();
+    }, []);
 
     const fetchTableData = () => {
         axios.get(`http://localhost:8080/tables/table?numberOfRows=${rowsToShow}`)
@@ -19,19 +23,39 @@ export const TableCreation = () => {
             });
     };
 
-    const handleRowsChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    const getServices = () => {
+        axios.get(`http://localhost:8080/services/user/802`)
+            .then(response => {
+                const serviceData = response.data;
+                const service = serviceData.map((service: any) => service.name);
+                setServices(service);
+            })
+            .catch(error => {
+                console.error(error);
+            });
+    }
+
+
+
+    const selectRowsNumber = (event: React.ChangeEvent<HTMLSelectElement>) => {
         setRowsToShow(Number(event.target.value));
     };
 
-    const selectService = (event: React.ChangeEvent<HTMLSelectElement>) => {
-        setService(event.target.value);
-    }
+    // const selectService = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    //     setServices((event.target.value));
+    // }
 
     return (
         <div>
             <h1>Table</h1>
+            <label htmlFor="services">Select Service:</label>
+            <select id="services" value={services}>
+                {services.map((service: any, index: number) => (
+                    <option key={index} value={service}>{service}</option>
+                ))}
+            </select>
             <label htmlFor="rows">Show rows:</label>
-            <select id="rows" value={rowsToShow} onChange={handleRowsChange}>
+            <select id="rows" value={rowsToShow} onChange={selectRowsNumber}>
                 <option value={10}>10</option>
                 <option value={50}>50</option>
                 <option value={100}>100</option>
