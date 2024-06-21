8544ace6adff359acdefdf114abc01fb341d8f0a
Agnieszka Stasiak <242531@edu.p.lodz.pl>
2024-06-21 18:33:30 +0200

    crud for services on front works

--- client/src/components/EditService.tsx
+++ client/src/components/EditService.tsx
@@ -2,50 +2,47 @@ import React, {useEffect, useState} from "react";
 import "../styles/login.css";
 import "../styles/servicesForms.css";
 import axios from "axios";
-import {Link, useParams} from "react-router-dom";
+import {Link, useParams, useLocation} from "react-router-dom";
 import {getAuthToken} from "../validateUser";
 
 interface EditServiceProps {
     name: string;
     address: string;
-    port: string;
 }
 
-function EditService(props: EditServiceProps) {
-
+function EditService() {
     const {index} = useParams();
-    const [service, setService] = useState<EditServiceProps>({name: "", address: "", port: ""});
-    const handleChange = (e: any) => {
-        console.log(e.target.name);
-        console.log(e.target.value);
-        setService({...service, [e.target.name]: e.target.value});
-    }
+    const location = useLocation();
+    const { name, address } = location.state as EditServiceProps;
+    const [service, setService] = useState<EditServiceProps>({name: "", address: ""});
 
     useEffect(() => {
-        console.log(props.name);
-        console.log(props.address);
-        console.log(props.port);
+        setService({ name, address });
+    }, [name, address]);
 
-        setService({...props});
-    }, [props]);
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const { name, value } = e.target;
+        setService(prevService => ({
+            ...prevService,
+            [name]: value
+        }));
+    };
 
     const editService = () => {
         axios.put(`http://localhost:8080/service/${index}`, service, {
             headers: {
                 "Content-Type": "application/json",
-                'Authorization': "Bearer " + getAuthToken() as string
+                'Authorization': "Bearer " + getAuthToken()
             }
         })
             .then(response => {
-                console.log("Login complete");
                 if (response.status === 200) {
                     window.location.href = '/dashboard';
                 }
-                console.log(getAuthToken());
             })
             .catch(error => {
                 console.error(error);
-            })
+            });
     }
 
     return (
@@ -54,21 +51,22 @@ function EditService(props: EditServiceProps) {
                 <h1 className="mt-5 ms-5 align-self-start">EDIT SERVICE</h1>
                 <input
                     onChange={handleChange}
+                    value={service.name}
                     className="form-control username mt-5"
                     placeholder="New name"
-                    type="text" name="name" id="name"/>
-
+                    type="text"
+                    name="name"
+                    id="name"
+                />
                 <input
                     onChange={handleChange}
+                    value={service.address}
                     className="form-control password mt-4"
                     placeholder="New address"
-                    type="text" name="address" id="addr"/>
-
-                <input
-                    onChange={handleChange}
-                    className="form-control password mt-4"
-                    placeholder="New port"
-                    type="text" name="port" id="por"/>
+                    type="text"
+                    name="address"
+                    id="addr"
+                />
                 <div className="row mt-5">
                     <div className="col">
                         <button
